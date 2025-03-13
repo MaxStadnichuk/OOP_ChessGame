03d443060a1867f7df1c62cc2e45c8d2e72641aa
Zentorn001 <mstdnchk@gmail.com>
2025-03-13 20:14:56 +0100

    Add Cell class with position, piece, and color attributes

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -1,4 +1,47 @@
 package game;
 
+import game.pieces.Piece;
+import util.Position;
+
 public class Cell {
+    private Position position;
+    private Piece piece;
+    private Color color;
+
+    public void clear() {
+        this.piece = null;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public boolean isEmpty() {
+        return piece == null;
+    }
+
+    @Override
+    public String toString() {
+        return "Cell{" + "position=" + position + ", piece=" + piece + '}';
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
